17530509ae7b3299cf019fd48db4171b59ee3036
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-07 14:08:41 +0530

    Successful Sign In Google with Google Button

--- client/src/Pages/SignIn.js
+++ client/src/Pages/SignIn.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import btnStyle from './ButtonStyle.css'
 import GoogleLogo from './GOOG-0ed88f7c.png'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
+
+
 const SignIn = () => {
 
     return (
+        <>
+            <GoogleOAuthProvider clientId="466752079259-k91pivjfensmgs2n52gd5d8898k0rsp5.apps.googleusercontent.com">
+                <GoogleLogin
+                    onSuccess={(credintialRespnse) => {
+                        console.log(credintialRespnse)
+                    }}
 
-        <button className="btnSignIn">
-            <img src={GoogleLogo} />
-            Sign In with Google
-        </button>
+                    onError={(err) => {
+                        console.log(err)
+                    }}
+                />
+
+            </GoogleOAuthProvider>
+
+            <button className="btnSignIn">
+                <img src={GoogleLogo} />
+                Sign In with Google
+            </button>
+        </>
+
 
     )
 }
