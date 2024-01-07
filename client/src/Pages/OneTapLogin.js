d4833380c5ffc3afcca8391ec4b94d642d8cf403
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-07 23:26:29 +0530

    OneTapLogin

---
+++ client/src/Pages/OneTapLogin.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useGoogleOneTapLogin } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
+
+const OneTapLogin = () => {
+    const successSignIn = (response) => {
+        console.log((response))
+        console.log(jwtDecode(response.access_token))
+    }
+
+    useGoogleOneTapLogin({
+        onError: error => console.log(error),
+        onSuccess: response => console.log(response),
+        googleAccountConfigs: {
+            client_id: "466752079259-k91pivjfensmgs2n52gd5d8898k0rsp5.apps.googleusercontent.com"
+        }
+    });
+
+    return (
+        <></>
+    )
+}
+
+
+export default OneTapLogin
